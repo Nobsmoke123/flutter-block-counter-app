78e387b3ce343ab52816e470c2d019ccc2b48621
Nobsmoke123 <donaldakobundu@gmail.com>
2020-06-13 11:20:55 +0100

    Learning state management using BLoc

---
+++ android/app/src/main/kotlin/com/cloudmallng/bloccounterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cloudmallng.bloccounterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import './core/counter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterBloc>(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  MyHomePage({this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (context, count){
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$count',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: (){
+                        counterBloc.add(CounterEvent.increment);
+                      },
+                      child: Text(
+                          'Increment'
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: (){
+                        counterBloc.add(CounterEvent.decrement);
+                      },
+                      child: Text(
+                          'Decrement'
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
